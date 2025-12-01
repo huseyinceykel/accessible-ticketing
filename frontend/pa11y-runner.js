34fedb3f8054a0675a69744d94cb964966187a18
huseyinCYKL <huseyinerenceykel@gmail.com>
2025-12-01 05:52:28 +0300

    update

--- frontend/pa11y-runner.js
+++ frontend/pa11y-runner.js
@@ -1,31 +1,15 @@
-import pa11y from "pa11y";
-import fs from "fs/promises";
+import pa11y from 'pa11y';
+import fs from 'fs';
 
 const run = async () => {
-  const results = await pa11y("http://localhost:3000", {
+  const results = await pa11y('http://localhost:3000', {
     chromeLaunchConfig: {
-      args: ["--no-sandbox", "--disable-setuid-sandbox"]
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
     },
-    standard: "WCAG2AA",
-    includeNotices: true,
-    includeWarnings: true
+    reporter: 'html'
   });
 
-  const htmlReport = `
-    <html>
-      <head>
-        <meta charset="UTF-8" />
-        <title>WCAG Accessibility Report</title>
-      </head>
-      <body>
-        <h1>WCAG Accessibility Report</h1>
-        <pre>${JSON.stringify(results, null, 2)}</pre>
-      </body>
-    </html>
-  `;
-
-  await fs.writeFile("wcag-report.html", htmlReport);
-  console.log("WCAG report generated: wcag-report.html");
+  fs.writeFileSync('wcag-report.html', results);
 };
 
 run();
